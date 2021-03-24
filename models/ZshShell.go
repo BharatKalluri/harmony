10f20edb27ba7ad845e6add6dfdb456f5e40d9dd
Bharat Kalluri <bharatkalluri@protonmail.com>
2021-03-24 23:46:45 +0530

    Fix ZSH parsing issues

--- models/ZshShell.go
+++ models/ZshShell.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"strconv"
 	"strings"
@@ -13,10 +15,10 @@ func (z ZSHShell) DecodeHistoryItem(encodedHistoryString string) (HistoryItem, e
 		fmt.Println("Invalid history line: ", encodedHistoryString)
 		return HistoryItem{}, nil
 	}
-	splitOnSemiColon := strings.Split(encodedHistoryString, ";")
+	splitOnSemiColon := strings.Split(strings.TrimSpace(encodedHistoryString), "0;")
 	cmdInHistory := splitOnSemiColon[len(splitOnSemiColon)-1]
 	splitOnColon := strings.Split(encodedHistoryString, ":")
-	timeStampStr := splitOnColon[0]
+	timeStampStr := strings.TrimSpace(splitOnColon[1])
 	timeStamp, err := strconv.Atoi(strings.TrimSpace(timeStampStr))
 	if err != nil {
 		return HistoryItem{}, err
@@ -32,9 +34,25 @@ func (z ZSHShell) EncodeHistoryItem(historyItem HistoryItem) string {
 }
 
 func (z ZSHShell) GetShellHistoryFromBytes(shellHistory []byte) (ShellHistory, error) {
-	historyItemsStrArr := strings.Split(string(shellHistory), ": ")
+
+	// remove escaped new lines and all \ chars, will flatten all history entries to one line
+	scanner := bufio.NewScanner(
+		bytes.NewReader(
+			bytes.ReplaceAll(
+				bytes.ReplaceAll(shellHistory, []byte("\\\n"), []byte("")),
+				[]byte("\\"),
+				[]byte(""),
+			),
+		),
+	)
+	var historyItemStrArr []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		historyItemStrArr = append(historyItemStrArr, line)
+	}
+
 	var historyItemsArr []HistoryItem
-	for _, el := range historyItemsStrArr {
+	for _, el := range historyItemStrArr {
 		if len(el) > 1 {
 			historyItem, err := z.DecodeHistoryItem(el)
 			if err != nil {
@@ -43,5 +61,6 @@ func (z ZSHShell) GetShellHistoryFromBytes(shellHistory []byte) (ShellHistory, e
 			historyItemsArr = append(historyItemsArr, historyItem)
 		}
 	}
+
 	return ShellHistory{History: historyItemsArr}, nil
 }
